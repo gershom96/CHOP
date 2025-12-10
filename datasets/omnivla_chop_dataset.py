Fixed normalization issue

datasets/omnivla_chop_dataset.py
@@ -62,6 +62,7 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
         max_dist: float = 30.0,
         modality_choices=(4, 5, 6),
         normalize: bool = True,
+        create_image_cache: bool = True
     ) -> None:
         super().__init__()
         
@@ -81,6 +82,8 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
         self.data_split_folder = data_split_folder
         self.data_split_type = data_split_type
         self.dataset_name = dataset_name
+        self.create_image_cache = create_image_cache
+
         self.trajectory_cache, self._image_cache = self._load_index(self.dataset_path)
         self.max_dist = max_dist
 
@@ -114,7 +117,7 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
 
     def __setstate__(self, state):
         self.__dict__ = state
-        self._build_caches()
+        # self._build_caches()
 
     def _get_image_cache(self):
         # Opens LMDB lazily. When called inside a worker, this creates a worker-local env.
@@ -139,8 +142,8 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
                 return img_path_to_data(image_path, self.image_size)
             return img_path_to_data(io.BytesIO(buf), self.image_size)
         except Exception as e:
-            print(f"Failed to load image {image_path}: {e}")
-            return None
+            # print(f"Failed to load image {image_path}: {e}")
+            return img_path_to_data(image_path, self.image_size)
 
     def _process_actions(self, path_data: Dict[str, torch.Tensor]):
         pts = path_data.get("points", torch.empty(0, 3, dtype=torch.float32))
@@ -161,7 +164,7 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
         if self.normalize:
             metric_spacing = self.data_config.get("metric_waypoint_spacing", 1.0)
             # print( "metric_spacing:", metric_spacing )
-            actions = actions / (metric_spacing * self.waypoint_spacing)
+            actions[:, :2] /= (metric_spacing * self.waypoint_spacing)
         return actions.float()
         
     def _compute_actions(self, action_data: Dict[str, torch.Tensor], 
@@ -172,6 +175,11 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
         neg_actions = self._process_actions(action_data.get("path_1", {}))
 
         goal_pos = to_local_coords_tensor(goal_pos, current_pos, current_yaw)
+
+        if self.normalize:
+            # Normalizing pos_actions and neg_actions already done in _process_actions
+            metric_spacing = self.data_config.get("metric_waypoint_spacing", 1.0)
+            goal_pos /= (metric_spacing * self.waypoint_spacing)
         goal_yaw_loc = goal_yaw - current_yaw
 
         return pos_actions, neg_actions, goal_pos, goal_yaw_loc
@@ -182,7 +190,8 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
 
         trajectory_cache = self._build_trajectory_cache(raw)
         print("Cached trajectories:", len(trajectory_cache))
-        image_cache = self._build_image_cache(trajectory_cache)
+        if self.create_image_cache:
+            image_cache = self._build_image_cache(trajectory_cache)
         return trajectory_cache, image_cache
 
     def __len__(self) -> int:
@@ -209,59 +218,59 @@ class OmniVLAChopDataset(torch.utils.data.Dataset):
                 trajectory_cache.append(item)
         return trajectory_cache
 
-    # def _build_image_cache(self, trajectory_cache: List[Dict[str, Any]], use_tqdm: bool = True):
-    #     cache_filename = os.path.join(self.data_split_folder, f"dataset_{self.dataset_name}_{self.data_split_type}.lmdb")
-
-    #     # build the LMDB file if missing (write once)
-    #     if not os.path.exists(cache_filename):
-    #         tqdm_iterator = tqdm.tqdm(trajectory_cache, disable=not use_tqdm, dynamic_ncols=True,
-    #                               desc=f"Building LMDB cache for {self.dataset_name}")
-            
-    #         env = lmdb.open(cache_filename, map_size=2**40)
-    #         txn = env.begin(write=True)
-    #         try:
-    #             for i, frame in enumerate(tqdm_iterator):
-    #                 rel_path = frame["image_path"]
-    #                 image_path = str(self.image_root / rel_path)
-
-    #                 with open(image_path, "rb") as f:
-    #                     img_bytes = f.read()
-    #                 txn.put(image_path.encode(), img_bytes)
-
-    #                 # Commit every N images to avoid giant in-memory transactions
-    #                 if (i + 1) % 1000 == 0:
-    #                     txn.commit()
-    #                     txn = env.begin(write=True)
-    #             # final commit
-    #             txn.commit()
-    #         finally:
-    #             env.close()
-
-    #     self._image_cache_path = cache_filename
-    #     self._image_cache = None
-    #     return None
-
     def _build_image_cache(self, trajectory_cache: List[Dict[str, Any]], use_tqdm: bool = True):
         cache_filename = os.path.join(self.data_split_folder, f"dataset_{self.dataset_name}_{self.data_split_type}.lmdb")
-        
-        env = lmdb.open(cache_filename, map_size=2**40)  # existing file is fine
-        with env.begin(write=True) as txn:
-            tqdm_iterator = tqdm.tqdm(trajectory_cache, disable=not use_tqdm, dynamic_ncols=True, desc=f"Filling LMDB for {self.dataset_name}")
-            for i, frame in enumerate(tqdm_iterator):
-                image_path = str(self.image_root / frame["image_path"])
-                key = image_path.encode()
-                if txn.get(key) is not None:
-                    continue  # already cached
-                with open(image_path, "rb") as f:
-                    txn.put(key, f.read())
-                if (i + 1) % 1000 == 0:
-                    txn.commit()
-                    txn = env.begin(write=True)
-            txn.commit()
-        env.close()
+        print("LMDB cache filename:", cache_filename)
+        # build the LMDB file if missing (write once)
+        if not os.path.exists(cache_filename):
+            tqdm_iterator = tqdm.tqdm(trajectory_cache, disable=not use_tqdm, dynamic_ncols=True,
+                                  desc=f"Building LMDB cache for {self.dataset_name}")
+            
+            env = lmdb.open(cache_filename, map_size=2**40)
+            txn = env.begin(write=True)
+            try:
+                for i, frame in enumerate(tqdm_iterator):
+                    rel_path = frame["image_path"]
+                    image_path = str(self.image_root / rel_path)
+
+                    with open(image_path, "rb") as f:
+                        img_bytes = f.read()
+                    txn.put(image_path.encode(), img_bytes)
+
+                    # Commit every N images to avoid giant in-memory transactions
+                    if (i + 1) % 1000 == 0:
+                        txn.commit()
+                        txn = env.begin(write=True)
+                # final commit
+                txn.commit()
+            finally:
+                env.close()
+
         self._image_cache_path = cache_filename
         self._image_cache = None
         return None
+
+    # def _build_image_cache(self, trajectory_cache: List[Dict[str, Any]], use_tqdm: bool = True):
+    #     cache_filename = os.path.join(self.data_split_folder, f"dataset_{self.dataset_name}_{self.data_split_type}.lmdb")
+        
+    #     env = lmdb.open(cache_filename, map_size=2**40)  # existing file is fine
+    #     with env.begin(write=True) as txn:
+    #         tqdm_iterator = tqdm.tqdm(trajectory_cache, disable=not use_tqdm, dynamic_ncols=True, desc=f"Filling LMDB for {self.dataset_name}")
+    #         for i, frame in enumerate(tqdm_iterator):
+    #             image_path = str(self.image_root / frame["image_path"])
+    #             key = image_path.encode()
+    #             if txn.get(key) is not None:
+    #                 continue  # already cached
+    #             with open(image_path, "rb") as f:
+    #                 txn.put(key, f.read())
+    #             if (i + 1) % 1000 == 0:
+    #                 txn.commit()
+    #                 txn = env.begin(write=True)
+    #         txn.commit()
+    #     env.close()
+    #     self._image_cache_path = cache_filename
+    #     self._image_cache = None
+    #     return None
 
     def _convert_path(self, path_data: Dict[str, Any]) -> Dict[str, torch.Tensor]:
         

datasets/vint_gnm_chop_dataset.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Optional, Tuple
 import tqdm
 import io
 import lmdb
+from collections import defaultdict
 
 import torch
 from torch.utils.data import Dataset
@@ -144,6 +145,7 @@ class ViNTChopDataset(Dataset):
         self.dataset_index = dataset_names.index(self.dataset_name)
         self.data_config = all_data_config[self.dataset_name]
         self.trajectory_cache = {}
+        self.bag_idx_img_ptr = {}
         self._load_index()
         
         if self.learn_angle:
@@ -179,18 +181,19 @@ class ViNTChopDataset(Dataset):
                 item["timestamp"] = sample.get("timestamp")
                 item["frame_idx"] = sample.get("frame_idx")
 
-                if item["frame_idx"] < self.context_size * self.waypoint_spacing
-                    continue  # not enough context
-                item["path_0"] = self._convert_path(sample.get("path_0", {}))
-                item["path_1"] = self._convert_path(sample.get("path_1", {}))
-                if "position" in sample and sample.get("position") is not None:
-                    item["position"] = torch.as_tensor(sample.get("position"), dtype=torch.float32)
-                if "yaw" in sample and sample["yaw"] is not None:
-                    item["yaw"] = torch.as_tensor(sample["yaw"], dtype=torch.float32)
-                item["stop"] = bool(sample.get("stop", False))
-                item["image_path"] = sample.get("image_path")
+                if item["frame_idx"] >= self.context_size * self.waypoint_spacing:
+                      # not enough context
+                    item["path_0"] = self._convert_path(sample.get("path_0", {}))
+                    item["path_1"] = self._convert_path(sample.get("path_1", {}))
+                    if "position" in sample and sample.get("position") is not None:
+                        item["position"] = torch.as_tensor(sample.get("position"), dtype=torch.float32)
+                    if "yaw" in sample and sample["yaw"] is not None:
+                        item["yaw"] = torch.as_tensor(sample["yaw"], dtype=torch.float32)
+                    item["stop"] = bool(sample.get("stop", False))
+                    item["image_path"] = sample.get("image_path")
 
-                trajectory_cache.append(item)
+                    trajectory_cache.append(item)
+                self.bag_idx_img_ptr[(bag_name, item["frame_idx"])] = sample.get("image_path")
         return trajectory_cache
 
     def _build_image_cache(self, trajectory_cache: List[Dict[str, Any]], use_tqdm: bool = True):
@@ -331,8 +334,9 @@ class ViNTChopDataset(Dataset):
             yaws = yaws.squeeze()
         
         assert pts.shape[0] == yaws.shape[0], "Points and yaws must have the same number of waypoints"
-        actions = pts[:, :2]  # ego-frame waypoints (x,y)
-
+        actions = pts[:self.len_traj_pred*self.waypoint_spacing + 1, :2]  # ego-frame waypoints (x,y)
+        yaws = yaws[:self.len_traj_pred*self.waypoint_spacing + 1]  # ego-frame yaws
+        
         if self.learn_angle and yaws.numel() > 0:
             yaw_col = yaws.reshape(-1, 1)
             actions = torch.cat([actions, yaw_col], dim=1)
@@ -355,15 +359,6 @@ class ViNTChopDataset(Dataset):
 
         return pos_actions, neg_actions, goal_pos, goal_yaw_loc
     
-    def _get_trajectory(self, trajectory_name):
-        if trajectory_name in self.trajectory_cache:
-            return self.trajectory_cache[trajectory_name]
-        else:
-            with open(os.path.join(self.data_folder, trajectory_name, "traj_data.pkl"), "rb") as f:
-                traj_data = pickle.load(f)
-            self.trajectory_cache[trajectory_name] = traj_data
-            return traj_data
-
     def __len__(self) -> int:
         return len(self.trajectory_cache)
 
@@ -381,49 +376,32 @@ class ViNTChopDataset(Dataset):
         """
         sample = self.trajectory_cache[i]
         cur_pos = sample["position"][:2]                            # w.r.t. world frame
-        cur_yaw = sample["yaw"]                                     # w.r.t. world frame
-        image_path = str(self.image_root / sample["image_path"])
-        cur_image = self._load_image(image_path)
+        cur_yaw = sample["yaw"]
+        curr_bag = sample["bag"]
+        curr_frame_idx = sample["frame_idx"]
 
-        # f_curr, curr_time, max_goal_dist = self.index_to_data[i]
-        
         goal = self._get_goal(i)
         goal_pos = goal["goal_position"]
         goal_yaw = goal["goal_yaw"]
         goal_image = goal["goal_image"]
 
-        f_goal, goal_time, goal_is_negative = 
-
         # Load images
         context = []
         if self.context_type == "temporal":
             # sample the last self.context_size times from interval [0, curr_time)
-            context_times = list(
+            context = list(
                 range(
-                    curr_time + -self.context_size * self.waypoint_spacing,
-                    curr_time + 1,
+                    curr_frame_idx + -self.context_size * self.waypoint_spacing,
+                    curr_frame_idx + 1,
                     self.waypoint_spacing,
                 )
             )
-            context = [(f_curr, t) for t in context_times]
         else:
             raise ValueError(f"Invalid context type {self.context_type}")
 
         obs_image = torch.cat([
-            self._load_image(f, t) for f, t in context
+            self._load_image(self.bag_idx_img_ptr[(curr_bag, t)]) for t in context
         ])
-
-        # Load goal image
-        goal_image = self._load_image(f_goal, goal_time)
-
-        # Load other trajectory data
-        curr_traj_data = self._get_trajectory(f_curr)
-        curr_traj_len = len(curr_traj_data["position"])
-        assert curr_time < curr_traj_len, f"{curr_time} and {curr_traj_len}"
-
-        goal_traj_data = self._get_trajectory(f_goal)
-        goal_traj_len = len(goal_traj_data["position"])
-        assert goal_time < goal_traj_len, f"{goal_time} an {goal_traj_len}"
 
         # Compute actions
         actions, goal_pos = self._compute_actions(curr_traj_data, curr_time, goal_time)

training/finetune-omnivla.py
@@ -108,7 +108,7 @@ transform = transforms.Compose([
 @dataclass
 class OmniVLAConfig:
     # fmt: off
-    vla_path: str = "/home/beast-gamma/Documents/GAMMA/Projects/weights/omnivla-finetuned-cast"             # Path to OpenVLA model (on HuggingFace Hub or stored locally)
+    vla_path: str = "./weights/omnivla-finetuned-cast"             # Path to OpenVLA model (on HuggingFace Hub or stored locally)
 
     # Dataset
     data_root_dir: Path = Path("datasets/rlds")      # Directory containing RLDS datasets
@@ -120,20 +120,20 @@ class OmniVLAConfig:
     num_images_in_input: int = 2                     # Number of images in the VLA input (default: 1)
 
     # Training configuration
-    batch_size: int = 1                              # Batch size per device (total batch size = batch_size * num GPUs)
+    batch_size: int = 4                              # Batch size per device (total batch size = batch_size * num GPUs)
     learning_rate: float = 1e-4                      # Learning rate
     lr_warmup_steps: int = 0                         # Number of steps to warm up learning rate (from 10% to 100%)
     num_steps_before_decay: int = 100_000            # Number of steps before LR decays by 10x
     grad_accumulation_steps: int = 10                # Number of gradient accumulation steps
     max_steps: int = 200_000                         # Max number of training steps
-    save_freq: int = 1000                          # Checkpoint saving frequency in steps    
+    save_freq: int = 10000                          # Checkpoint saving frequency in steps    
     save_latest_checkpoint_only: bool = False        # If True, saves only 1 checkpoint, overwriting latest checkpoint
                                                      #   (If False, saves all checkpoints)
     image_aug: bool = True                           # If True, trains with image augmentations (HIGHLY RECOMMENDED)
 
     # LoRA
     use_lora: bool = True                            # If True, uses LoRA fine-tuning
-    lora_rank: int = 8                              # Rank of LoRA weight matrix
+    lora_rank: int = 32                              # Rank of LoRA weight matrix
     lora_dropout: float = 0.0                        # Dropout applied to LoRA weights
     merge_lora_during_training: bool = True          # If True, merges LoRA weights and saves result during training
                                                      #   Note: Merging can be very slow on some machines. If so, set to
@@ -391,7 +391,7 @@ def run_forward_pass(
         
         loss = (
             torch.nn.MSELoss()(action_ref, predicted_actions)
-            - 0.1 * torch.nn.MSELoss()(neg_action_ref, predicted_actions)
+            # - 0.1 * torch.nn.MSELoss()(neg_action_ref, predicted_actions)
             + 0.1 * obj_loss
             + 0.1 * torch.nn.MSELoss()(predicted_actions[:, 0:-1], predicted_actions[:, 1:])
         )
@@ -923,7 +923,8 @@ def train_omnivla(cfg: OmniVLAConfig) -> None:
             learn_angle=config["learn_angle"],
             normalize=config["normalize"],
             modality_choices=(4, 5, 6),
-        ) 
+            create_image_cache=True if data_split_type == "train" else False
+        )
 
         if data_split_type == "train":
             train_dataset.append(dataset_chop)